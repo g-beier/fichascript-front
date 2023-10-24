f0bac5483150d6368034933fc7aaf18e15facd37
g-beier <gustavbeier@gmail.com>
2023-10-23 23:58:58 -0300

    Testing Next.js App Routes and TailwindCSS integration

--- src/app/character/page.tsx
+++ src/app/character/page.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="grid min-h-screen place-items-center">
-      <h1 className="bg-red-950">Landing page</h1>
+      <h1>
+        Page - <strong className="font-bold">Character</strong>
+      </h1>
+      <Link href="/">Home</Link>
       <Link href="/login">Login</Link>
-      <Link href="/character">Character</Link>
     </main>
   );
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="grid min-h-screen place-items-center">
-      <h1 className="bg-red-950">Landing page</h1>
-      <Link href="/login">Login</Link>
+      <h1>
+        Page - <strong className="font-bold">Login</strong>
+      </h1>
+      <Link href="/">Home</Link>
       <Link href="/character">Character</Link>
     </main>
   );
